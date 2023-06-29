90cd60d48fce6d0c707ffbd56784e71028c96e69
Mansi-Tiwari <manutiwari0507@gmail.com>
2023-06-29 20:48:31 +0530

    removed error

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -5,25 +5,35 @@ import "./SearchBar.css";
 
 const SearchBar = ({ setOpen }) => {
   const [query, setQuery] = useState("");
+
   const navigate = useNavigate();
+
   const searchQueryHandler = (event) => {
     if (event.key === "Enter" && query.length > 0) {
       setOpen(false);
-      setQuery("")
+      setQuery("");
       navigate(`/search/${query}`);
+    }
   };
 
   const searchQueryOnClick = () => {
-    console.log(query)
+    console.log(query);
     if (query.length > 0) {
       setOpen(false);
-      setQuery("")
+      setQuery("");
       navigate(`/search/${query}`);
+    }
   };
   return (
     <div className="search-bar-container">
       <div className="input-wrapper items-center">
-        <img src={search} alt={search} id="search-icon" className="w-6 h-6"  onClick={searchQueryOnClick}  />
+        <img
+          src={search}
+          alt={search}
+          id="search-icon"
+          className="w-6 h-6"
+          onClick={searchQueryOnClick}
+        />
         &nbsp;&nbsp;
         <input
           className="text-black w-[160px]"
